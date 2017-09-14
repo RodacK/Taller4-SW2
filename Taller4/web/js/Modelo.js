854bcc608f1ef464a804aae5fddaed4c00b53081
elrober <elrober@elrober>
2017-09-14 14:02:49 -0500

    99%

---
+++ Taller4/web/js/Modelo.js
@@ -0,0 +1,29 @@
+var idFacultad=3;
+var facultades=[
+    {"id":1, "nombre":"Ingenieria"},
+    {"id":2, "nombre":"Licenciatura"}
+
+];
+var idCarrera=1;
+var carreras=[
+     {"id":1, "nombre":"Ingenieria de Sistemas y Computacion"},
+];
+
+var idMunicipio=4;
+var municipios=[
+    {"id":1, "nombre":"Tunja"},
+    {"id":2, "nombre":"Duitama"},
+    {"id":3, "nombre":"Sogamoso"},
+];
+
+var idProfesores=4;
+var profesores=[
+    {"id":1, "nombre":"Diego", "apellidos":"Torres", "documento":"1049651765"},
+    {"id":1, "nombre":"Alejandro", "apellidos":"Niño", "documento":"1049651766"},
+    {"id":1, "nombre":"Mariana", "apellidos":"Correa", "documento":"1049651767"},
+];
+
+var idMaterias=4;
+var materias=[
+    {"id":1, "nombre":"Software II", "creditos":"4",},
+];
